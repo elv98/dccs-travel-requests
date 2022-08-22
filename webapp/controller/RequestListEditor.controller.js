c2b610fa04d07e2e3f075dc9e0f26bcf22e65a96
elv98 <elvina.dautovic98@gmail.com>
2022-08-22 23:20:48 +0200

    Add editor page

---
+++ webapp/controller/RequestListEditor.controller.js
@@ -0,0 +1,80 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller",
+  "sap/ui/model/json/JSONModel",
+], function (Controller, JSONModel) {
+  
+  "use strict";
+  
+  var firebase;
+  var firestore; 
+  var auth;
+
+  return Controller.extend("dccstravelrequests.controller.RequestListEditor", {
+      
+    onInit: function(){
+      // Initialize firebase
+      firebase = this.getOwnerComponent().getModel("firebase")
+      firestore = firebase.getData().firestore;
+      auth = firebase.getData().auth;
+
+      // Start observing auth state    
+      auth.onAuthStateChanged(editor => {
+        if (editor) {
+          var unsubscribe= firestore.collection("travel-requests").where("approved","==","Approved").onSnapshot((querySnapshot) => {
+            let requests = []
+            querySnapshot.forEach((doc) => {
+              let travelRequest = {
+                id: doc.id,
+                data: doc.data()
+              }
+              requests.push(travelRequest)
+          });
+            let data = {
+              requests: requests
+            }
+            let oModel = new JSONModel(data);
+            this.getView().setModel(oModel);
+          });
+        } else {
+          if (unsubscribe) {
+            unsubscribe()
+          }
+          unsubscribe = null;
+        }
+      })
+    },  
+
+    /**
+    * Open to edit travel request
+    */
+    openTravelRequestEditView: function (oEvent) {
+      var oSelectedItemBinding = oEvent.getSource().getBindingContext()
+      if (!this.detailTravelRequestDialog) {
+        this.detailTravelRequestDialog = sap.ui.xmlfragment(
+          "dccstravelrequests.view.DetailViewEditor",
+          this
+        );
+      }
+      this.detailTravelRequestDialog.setBindingContext(oSelectedItemBinding);
+      this.getView().addDependent(this.detailTravelRequestDialog);
+      this.detailTravelRequestDialog.open();
+    },
+
+    /**
+    * Save edited travel request
+    */
+    onSaveButtonPress: function (oEvent) {
+      let oCurrentRequest = oEvent.getSource().getBindingContext().getObject().data;
+      firestore.collection("travel-requests").doc(oCurrentRequest.id).update(oCurrentRequest);
+      this.detailTravelRequestDialog.close();
+    },
+      
+    /**
+    * Close edit travel request button
+    */
+    onCloseEditView: function () {
+      this.detailTravelRequestDialog.close();
+    },
+  });
+});
+  
