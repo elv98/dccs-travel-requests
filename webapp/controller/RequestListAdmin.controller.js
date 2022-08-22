159139348ce7d54af81833aa3a55a9f78bdf0db3
elv98 <elvina.dautovic98@gmail.com>
2022-08-22 23:21:22 +0200

    Add admin page

---
+++ webapp/controller/RequestListAdmin.controller.js
@@ -0,0 +1,98 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller",
+  "sap/ui/model/json/JSONModel",
+ 
+],function (Controller,JSONModel,) {
+    
+  "use strict";
+
+  var firebase;
+  var firestore; 
+  var auth;
+
+  return Controller.extend("dccstravelrequests.controller.RequestList", {
+
+    onInit: function () {
+      // Initialize firebase
+      firebase = this.getOwnerComponent().getModel("firebase")
+      firestore = firebase.getData().firestore;
+      auth = firebase.getData().auth;
+
+      // Start observing auth state    
+      auth.onAuthStateChanged(admin => {
+        if (admin) {
+          var unsubscribe= firestore.collection("travel-requests").orderBy("approved", "desc").onSnapshot((querySnapshot) => {
+            let requests = []
+            querySnapshot.forEach((doc) => {
+              let travelRequest = {
+                id: doc.id,
+                data: doc.data()
+              }
+              requests.push(travelRequest)
+          });
+            let data = {
+              requests: requests
+            }
+            let oModel = new JSONModel(data);
+            this.getView().setModel(oModel);
+          });
+        } else {
+          if (unsubscribe) {
+            unsubscribe()
+          }
+          unsubscribe = null;
+        }
+      })
+    },
+
+    /**
+    * Button for Approve new request 
+    */
+    onApproveButton: function(oEvent){
+      let  travelRequest = oEvent.getSource().getBindingContext().getObject();
+      this.detailTravelRequestDialog.close();
+      firestore.collection("travel-requests").doc(travelRequest.id).update({
+        approved: "Approved",
+      });
+    },
+   
+    /**
+    * Button for Denied new request user
+    */
+    onDeniedButton: function(oEvent){
+      var travelRequest= oEvent.getSource().getBindingContext().getObject();
+      this.detailTravelRequestDialog.close();
+      firestore.collection("travel-requests").doc(travelRequest.id).update({
+      approved: "Denied",
+      })
+    },
+   
+    /**
+    * Close view travel request button
+    */
+    onCloseDetailView: function () {
+     this.detailTravelRequestDialog.close();
+    },
+   
+
+    onCloseStatusView :function () {
+      this.dialogStatus.close();
+    },
+
+    /**
+    * Open travel request detail view
+    */
+    openTravelRequestDetailView: function (oEvent) {
+      var oSelectedItemBinding = oEvent.getSource().getBindingContext();
+      if (!this.detailTravelRequestDialog) {
+         this.detailTravelRequestDialog = sap.ui.xmlfragment(
+          "dccstravelrequests.view.DetailViewAdmin",
+          this
+        );
+      }
+      this.detailTravelRequestDialog.setBindingContext(oSelectedItemBinding);
+      this.getView().addDependent(this.detailTravelRequestDialog) 
+      this.detailTravelRequestDialog.open();
+    },
+  });
+});
